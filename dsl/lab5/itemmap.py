cf9a12f4f4110746e3d9e7bd67ce06f8330325c2
altair-1 <98402213+altair-1@users.noreply.github.com>
2024-02-08 11:15:41 +0530

    Add files via upload

---
+++ dsl/lab5/itemmap.py
@@ -0,0 +1,6 @@
+import fileinput
+for line in fileinput.input():
+    data = line.strip().split("\t")
+    if len(data) == 6:
+        date, time, location, item, cost, payment = data
+        print("{}\t{}".format(payment, cost))
---
+++ dsl/lab5/itemred.py
@@ -0,0 +1,11 @@
+import fileinput
+transaction_count = 0
+sales_total = 0
+for line in fileinput.input():
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        continue
+    current_key, current_value = data
+    transaction_count += 1
+    sales_total += float(current_value)
+print("{}\t{}".format(transaction_count, sales_total))
