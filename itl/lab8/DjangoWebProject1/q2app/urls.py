6865ed5da12f88c4feca8a6135b9fb015a0b9e83
altair-1 <98402213+altair-1@users.noreply.github.com>
2024-05-21 09:35:05 +0530

    Add files via upload

---
+++ itl/lab8/DjangoWebProject1/q2app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('firstPage/',views.home,name='firstpage'),
+    path('secondPage/',views.display,name='secondPage'),
+]
